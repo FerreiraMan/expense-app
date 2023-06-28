e2676dd0757090e72b40cac42dddc6f81225c174
pedro.sferreira <ferreirapedro.sjm@gmail.com>
2023-06-28 14:28:33 +0100

    First commit

---
+++ src/main/java/in/pferreira/expenseappproject/ExpenseAppProjectApplication.java
@@ -0,0 +1,13 @@
+package in.pferreira.expenseappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/pferreira/expenseappproject/ExpenseAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pferreira.expenseappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
