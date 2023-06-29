30ce6a2ad3c3604a5e6b2fdbc5ba947046eda12a
pedro.sferreira <ferreirapedro.sjm@gmail.com>
2023-06-29 14:52:02 +0100

    added update expense functionality

--- src/main/java/in/pferreira/expenseappproject/controller/ExpenseController.java
+++ src/main/java/in/pferreira/expenseappproject/controller/ExpenseController.java
@@ -28,4 +28,9 @@ public class ExpenseController {
 
     @PostMapping("/expenses")
     public Expense saveExpenseDetails(@RequestBody Expense expense){ return expenseService.saveExpenseDetails(expense);}
+
+    @PutMapping("/expenses/{id}")
+    public Expense updateExpenseDetails(@RequestBody Expense expense, @PathVariable Long id) {
+        return expenseService.updateExpenseDetails(id, expense);
+    }
 }
