3cc1850a5d629f262bb6971801b6097753a05cd4
pedro.sferreira <ferreirapedro.sjm@gmail.com>
2023-06-29 15:45:34 +0100

    added timestamps to expense identity: created and updated at

--- src/main/java/in/pferreira/expenseappproject/entity/Expense.java
+++ src/main/java/in/pferreira/expenseappproject/entity/Expense.java
@@ -4,9 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
@@ -30,4 +33,11 @@ public class Expense {
 
     private Date date;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
