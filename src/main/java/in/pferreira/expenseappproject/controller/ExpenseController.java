5fb789c9f2080cc4764fa376c9b58199fc9d1e3a
pedro.sferreira <ferreirapedro.sjm@gmail.com>
2023-07-12 15:05:15 +0100

    add custom exceptions and validations

--- src/main/java/in/pferreira/expenseappproject/controller/ExpenseController.java
+++ src/main/java/in/pferreira/expenseappproject/controller/ExpenseController.java
@@ -5,7 +5,6 @@ import in.pferreira.expenseappproject.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
