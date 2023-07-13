fbbf789e64344bfb64f2446fd0f0f34dc6213c6d
pedro.sferreira <ferreirapedro.sjm@gmail.com>
2023-07-13 11:45:17 +0100

    add custom exception for already used email

--- src/main/java/in/pferreira/expenseappproject/service/UserServiceImpl.java
+++ src/main/java/in/pferreira/expenseappproject/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package in.pferreira.expenseappproject.service;
 
 import in.pferreira.expenseappproject.entity.User;
 import in.pferreira.expenseappproject.entity.UserModel;
+import in.pferreira.expenseappproject.exceptions.ItemAlreadyExistsException;
 import in.pferreira.expenseappproject.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(UserModel user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new ItemAlreadyExistsException("Email is already used:"+user.getEmail());
+        }
         User newUser = new User();
         BeanUtils.copyProperties(user, newUser);
         return userRepository.save(newUser);
