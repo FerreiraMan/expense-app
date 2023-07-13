91128743af537618b55280bc1c6aab12db6f8e4c
pedro.sferreira <ferreirapedro.sjm@gmail.com>
2023-07-13 17:26:26 +0100

    restrict http requests except on /login and /register

--- src/main/java/in/pferreira/expenseappproject/controller/UserController.java
+++ src/main/java/in/pferreira/expenseappproject/controller/UserController.java
@@ -1,10 +1,7 @@
 package in.pferreira.expenseappproject.controller;
 
-import in.pferreira.expenseappproject.entity.Expense;
 import in.pferreira.expenseappproject.entity.User;
-import in.pferreira.expenseappproject.entity.UserModel;
 import in.pferreira.expenseappproject.service.UserService;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -19,10 +16,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<User> save(@Valid @RequestBody UserModel user) {
-        return new ResponseEntity<User>(userService.createUser(user), HttpStatus.CREATED);
-    }
     @GetMapping("/users/{id}")
     public ResponseEntity<User> readUser(@PathVariable Long id) {
         return new ResponseEntity<User>(userService.readUser(id), HttpStatus.OK);
