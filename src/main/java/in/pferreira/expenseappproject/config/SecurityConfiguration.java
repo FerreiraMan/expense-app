030f08eb0b1295fedb66b80e4c645365e05e0629
pedro.sferreira <ferreirapedro.sjm@gmail.com>
2023-07-17 17:33:05 +0100

    add login in functionality

--- src/main/java/in/pferreira/expenseappproject/config/SecurityConfiguration.java
+++ src/main/java/in/pferreira/expenseappproject/config/SecurityConfiguration.java
@@ -4,14 +4,21 @@ import in.pferreira.expenseappproject.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
--- src/main/java/in/pferreira/expenseappproject/controller/AuthController.java
+++ src/main/java/in/pferreira/expenseappproject/controller/AuthController.java
@@ -1,11 +1,17 @@
 package in.pferreira.expenseappproject.controller;
 
+import in.pferreira.expenseappproject.entity.AuthModel;
 import in.pferreira.expenseappproject.entity.User;
 import in.pferreira.expenseappproject.entity.UserModel;
 import in.pferreira.expenseappproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +22,16 @@ import javax.validation.Valid;
 public class AuthController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login() {
-        return new ResponseEntity<String>("User is logged in", HttpStatus.OK);
+    public ResponseEntity<HttpStatus> login(@RequestBody AuthModel authModel) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authModel.getEmail(), authModel.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
     }
     @PostMapping("/register")
     public ResponseEntity<User> save(@Valid @RequestBody UserModel user) {
